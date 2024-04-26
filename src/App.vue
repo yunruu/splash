f781a56418e0ab7d17b7b8c621c4e3d5e2fef1cb
Marvin Pranajaya <marvinpofficial@gmail.com>
2024-04-27 01:09:16 +0200

    First commit

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,10 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Slayyy!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Homey</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
