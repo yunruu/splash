1d890adeea2220d8108d4df05de813e4ee14ee5e
Marvin Pranajaya <marvinpofficial@gmail.com>
2024-04-27 12:16:59 +0200

    feat: check for env keys

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,15 +1,41 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
-export const firebaseApp = initializeApp({
-  // your application settings
-  apiKey: import.meta.env.VITE_FIREBASE_apiKey,
-  authDomain: import.meta.env.VITE_FIREBASE_authDomain,
-  projectId: import.meta.env.VITE_FIREBASE_projectId,
-  storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
-  appId: import.meta.env.VITE_FIREBASE_appId
-})
+let firebaseApp
+
+function checkEnvVariables() {
+  const requiredEnvVars = [
+      'VITE_FIREBASE_apiKey',
+      'VITE_FIREBASE_authDomain',
+      'VITE_FIREBASE_projectId',
+      'VITE_FIREBASE_storageBucket',
+      'VITE_FIREBASE_messagingSenderId',
+      'VITE_FIREBASE_appId'
+  ];
+
+  const missingVars = requiredEnvVars.filter(key => !import.meta.env[key]);
+  if (missingVars.length) {
+      console.error('Missing Firebase environment variables:', missingVars);
+      return false
+  }
+  return true
+}
+if (checkEnvVariables()) {
+  firebaseApp = initializeApp({
+    // your application settings  
+    apiKey: import.meta.env.VITE_FIREBASE_apiKey,
+    authDomain: import.meta.env.VITE_FIREBASE_authDomain,
+    projectId: import.meta.env.VITE_FIREBASE_projectId,
+    storageBucket: import.meta.env.VITE_FIREBASE_storageBucket,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_messagingSenderId,
+    appId: import.meta.env.VITE_FIREBASE_appId
+  })
+} else {
+  console.error("Environment Keys Missing")
+}
 
 // used for the firestore refs
-export const db = getFirestore(firebaseApp)
+const db = firebaseApp ? getFirestore(firebaseApp) : undefined
+
+// Exports
+export {firebaseApp, db}
