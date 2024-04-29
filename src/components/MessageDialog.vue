20a829c26fb1e6f7745e7d6ca01994bd2d11519b
yunruu <yunruuchin@gmail.com>
2024-04-29 21:56:11 +0200

    feat: create reusable message dialog ui

---
+++ src/components/MessageDialog.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="z-[2000] fixed top-0 left-0 right-0 m-4 bg-white p-4 rounded-lg shadow-md">
+        <div class="text-lg font-bold mb-4 border-b pb-3">{{ props.title || 'Untitled' }}</div>
+        <div class="text-sm text-gray-500">
+            {{ props.message }}
+        </div>
+
+        <div class="flex flex-row-reverse gap-4 mt-4">
+            <button
+                class="bg-rose-800 text-white px-4 py-2 rounded hover:cursor-pointer hover:bg-rose-900"
+                @click="confirm"
+            >
+                Confirm
+            </button>
+            <button
+                class="bg-gray-200 text-gray-800 px-4 py-2 rounded hover:cursor-pointer hover:bg-gray-300"
+                @click="cancel"
+            >
+                Cancel
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineEmits, defineProps } from 'vue';
+
+const props = defineProps(['title', 'message']);
+const emit = defineEmits(['confirm', 'cancel']);
+
+const confirm = () => {
+    emit('confirm');
+};
+
+const cancel = () => {
+    emit('cancel');
+};
+</script>
