a151f3ae5e5f25283e171bc3cd8bc0eee6fd66b3
yunruu <yunruuchin@gmail.com>
2024-04-29 21:04:30 +0200

    feat: route to group detail view on clicking card

--- src/views/GroupView.vue
+++ src/views/GroupView.vue
@@ -6,24 +6,26 @@
         :description="group.description"
         :members="group.profiles"
         class="mb-3"
+        @click="onClickViewGroup(group.id)"
     />
-    <div class="add-group-btn-wrapper">
-        <button
-            class="bg-rose-800 px-4 py-2 text-white text-lg font-semibold rounded-full"
-            @click="onClickCreateGroup"
-        >
-            + New group
-        </button>
-    </div>
+    <button
+        class="bg-rose-800 px-4 py-2 text-white text font-semibold rounded-full fixed bottom-20 right-8"
+        @click="onClickCreateGroup"
+    >
+        + New group
+    </button>
 
     <GroupFormCard v-if="isCreateModalOpen" @closeModal="closeGroupModal" />
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
 import { getGroups } from '../api/group';
 import GroupCard from '../components/GroupCard.vue';
 import GroupFormCard from '../components/GroupFormCard.vue';
+
+const router = useRouter();
 
 const groupsData = ref([]);
 const isCreateModalOpen = ref(false);
@@ -45,15 +47,11 @@ const fetchGroups = async () => {
     }
 };
 
+const onClickViewGroup = (groupId) => {
+    router.push(`/group/${groupId}`);
+};
+
 onMounted(async () => {
     await fetchGroups();
 });
 </script>
-
-<style scoped>
-.add-group-btn-wrapper {
-    position: fixed;
-    bottom: 80px;
-    right: 25px;
-}
-</style>
