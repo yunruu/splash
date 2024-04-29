68b9b3e13f16d09d10e6f88412cced45ba34ea6e
yunruu <yunruuchin@gmail.com>
2024-04-29 23:28:21 +0200

    feat: set prop to view cancel button

--- src/components/MessageDialog.vue
+++ src/components/MessageDialog.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="z-[2000] fixed top-0 left-0 right-0 m-4 bg-white p-4 rounded-lg shadow-md">
+    <div class="z-[9999] fixed top-0 left-0 right-0 m-4 bg-white p-4 rounded-lg shadow-md">
         <div class="text-lg font-bold mb-4 border-b pb-3">{{ props.title || 'Untitled' }}</div>
         <div class="text-sm text-gray-500">
             {{ props.message }}
@@ -13,6 +13,7 @@
                 Confirm
             </button>
             <button
+                v-if="props.isCancel"
                 class="bg-gray-200 text-gray-800 px-4 py-2 rounded hover:cursor-pointer hover:bg-gray-300"
                 @click="cancel"
             >
@@ -25,7 +26,7 @@
 <script setup>
 import { defineEmits, defineProps } from 'vue';
 
-const props = defineProps(['title', 'message']);
+const props = defineProps(['title', 'message', 'isCancel']);
 const emit = defineEmits(['confirm', 'cancel']);
 
 const confirm = () => {
