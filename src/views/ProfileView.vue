a56d466dfebf9829545af8af9df8100e36f7e984
yunruu <yunruuchin@gmail.com>
2024-04-29 23:29:40 +0200

    feat: display login form or page based on if user is logged in

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,8 +1,13 @@
 <script setup>
 import { getProfile } from '../api/profile';
 import { ref, onMounted } from 'vue';
+import { useStore } from 'vuex';
 
+import LoginFormCard from '../components/LoginFormCard.vue';
+
+const store = useStore();
 const profileData = ref({});
+const isLoginModalOpen = ref(false);
 
 const populateData = async (username) => {
     try {
@@ -15,12 +20,51 @@ const populateData = async (username) => {
 };
 
 onMounted(async () => {
-    profileData.value = await populateData('admin');
+    const user = store.state.username;
+    if (!user) {
+        isLoginModalOpen.value = true;
+    } else {
+        profileData.value = await populateData(user.username);
+        console.log(profileData.value);
+    }
 });
+
+const onLogin = async (username) => {
+    console.log(username);
+    profileData.value = await populateData(username);
+    isLoginModalOpen.value = false;
+};
 </script>
 
 <template>
-    <div class="profile">
-        <h1>Welcome back, {{ profileData?.name }}!</h1>
+    <LoginFormCard v-if="isLoginModalOpen" @logged-in="onLogin" />
+
+    <div v-else class="profile">
+        <h1 class="text-2xl font-bold py-3 border-b mb-12">
+            Welcome back, {{ profileData?.name }}
+        </h1>
+
+        <form class="flex flex-col gap-6">
+            <div class="flex flex-col gap-2">
+                <label for="name" class="text-xs font-semibold">Username</label>
+                <input
+                    id="name"
+                    type="text"
+                    v-model="profileData.username"
+                    class="border rounded-lg px-3 py-2"
+                    disabled
+                />
+            </div>
+            <div class="flex flex-col gap-2">
+                <label for="email" class="text-xs font-semibold">Email</label>
+                <input
+                    id="email"
+                    type="email"
+                    v-model="profileData.email"
+                    class="border rounded-lg px-3 py-2"
+                    disabled
+                />
+            </div>
+        </form>
     </div>
 </template>
