c7c45a915bb14813c8ee11dc8a9d8bdefa700eee
yunruu <yunruuchin@gmail.com>
2024-05-02 11:25:00 +0200

    feat: configure vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,47 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'auto',
+            includeAssets: [
+                'favicon.ico',
+                'apple-touch-icon.png',
+                'mask-icon.svg',
+                'favicon-32x32.png',
+                'favicon-16x16.png'
+            ],
+            manifest: {
+                name: '',
+                short_name: '',
+                icons: [
+                    {
+                        src: '/android-chrome-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/android-chrome-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ],
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                display: 'standalone'
+            }
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
-})
+});
